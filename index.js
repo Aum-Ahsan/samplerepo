ded34a1d2031222a2bea2c51df2de40256dbcc5b
AhsanAum <ahsanaum3@gmail.com>
2025-08-08 21:43:43 +0530

    all pages

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const heading = document.querySelector('h1');
     heading.addEventListener('click', function() {
-        alert('You clicked the heading!');
+        alert('You clicked the heading!11');
     });
 });
